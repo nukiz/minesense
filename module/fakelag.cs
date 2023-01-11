8aa2219f17f19363873b1255958ed37b1a05a5a6
Zyph <66350598+Dimwitted-Developer@users.noreply.github.com>
2023-01-11 11:48:09 -0800

    nukiz is a black monkey boy - Zyph

---
+++ module/fakelag.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Timers;
+using System.Threading;
+using System.Windows.Forms;
+using Memory;
+using System.Diagnostics;
+using WindowsFormsApp1.minesense.feature.Global;
+
+namespace WindowsFormsApp1.minesense.feature.module
+{
+    public class fakelag
+    {       
+        public static void Starter()
+        {
+            Random rnd = new Random();
+            Form1 form = new Form1();
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTick);
+
+            int minDelay = Convert.ToInt32(form.minFakeLagDelay.Value);
+            int maxDelay = Convert.ToInt32(form.maxFakeLagDelay.Value); //warning to end user has to be added - this WILL mess with discord and other network connections.
+
+            int resultedDelay = rnd.Next(minDelay, maxDelay);
+            var cmdProcess = Process.Start(GlobalVarHandler.cmdStartInfo);
+
+            if (form.FakeLagStart.Checked == true)
+            {               
+                aTimer.Enabled = true;
+                aTimer.Start(); 
+                aTimer.Interval = resultedDelay;
+            }
+            else
+            {
+                aTimer.Stop();
+                aTimer.Enabled = false;
+                cmdProcess.Kill();
+            }
+       }
+
+        
+        public static void OnTick(object source, ElapsedEventArgs e)
+        {
+            string black = "ipconfig /release";
+            string nigger = "ipconfig /renew";
+            Random rnd = new Random();
+            Form1 form = new Form1();
+            int minDelay = Convert.ToInt32(form.minFakeLagDelay.Value);
+            int maxDelay = Convert.ToInt32(form.maxFakeLagDelay.Value);
+            int resultedDelay = rnd.Next(minDelay, maxDelay);
+            var cmdProcess = Process.Start(GlobalVarHandler.cmdStartInfo);
+
+            Console.WriteLine(GlobalVarHandler.fakelag + "Resulted delay complete.");
+            Console.WriteLine(GlobalVarHandler.fakelag + "Delay amount approximately " + resultedDelay + "ms.");
+
+            cmdProcess.StandardInput.WriteLine(black); // we release internet connection           
+            Thread.Sleep(resultedDelay);
+            cmdProcess.StandardInput.WriteLine(nigger); // we renew the internet connection (restart) ik its a bad way to do this but idc
+        }
+
+        public static void LagSwitchHandler()
+        {
+            // we need to add a listener for a set keybind which i will do in the future
+            string black = "ipconfig /release";
+            string nigger = "ipconfig /renew";
+
+            Form1 form = new Form1();
+            var cmdProcess = Process.Start(GlobalVarHandler.cmdStartInfo);
+            int time = Convert.ToInt32(form.LagSwitchTime.Value * 1000);
+
+            cmdProcess.StandardInput.WriteLine(black);
+            Thread.Sleep(time);
+            cmdProcess.StandardInput.WriteLine(nigger);
+        }
+
+        public static void FakeLagFluctuation()
+        {
+            Form1 form = new Form1();
+            Random rnd = new Random();
+            int minDelay = Convert.ToInt32(form.minFakeLagDelay.Value);
+            int maxDelay = Convert.ToInt32(form.maxFakeLagDelay.Value); 
+            int resultedDelay = rnd.Next(minDelay, maxDelay);
+            System.Timers.Timer FlucCalculator = new System.Timers.Timer();
+            FlucCalculator.Elapsed += new ElapsedEventHandler(FlucHandler);
+            FlucCalculator.Interval = resultedDelay;
+
+            if (form.FluctuateCheck.Checked == true)
+            {
+                FlucCalculator.Enabled = true;
+                FlucCalculator.Start();
+            }
+            else
+            {
+                FlucCalculator.Enabled = false;
+                FlucCalculator.Stop();
+            }
+        }
+
+        public static void FlucHandler(object source, ElapsedEventArgs e)
+        {
+            System.Timers.Timer FlucCalculator = new System.Timers.Timer();
+            FlucCalculator.Elapsed += new ElapsedEventHandler(FlucHandler);
+            Mem m = new Mem();
+            Form1 form = new Form1();
+            Random rnd = new Random();
+            int minDelay = Convert.ToInt32(form.minFakeLagDelay.Value);
+            int maxDelay = Convert.ToInt32(form.maxFakeLagDelay.Value);
+            int FlucResultBefore = Convert.ToInt32(form.FlucAmountVal.Value);
+            int resultedDelay = rnd.Next(minDelay, maxDelay);
+
+            FlucCalculator.Interval = resultedDelay;
+
+            
+
+            int FlucResulted = resultedDelay * FlucResultBefore;
+            Console.WriteLine(GlobalVarHandler.fakelag + "Fluctuation applied.");
+            
+            if(form.ReachFluc.Checked == true)
+            {
+                if (m.ReadFloat("base+3FAE0D0") > 5)
+                {
+                    resultedDelay = FlucResulted;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else
+            {
+                return;
+            }
+
+        }
+
+    }
+}
---
+++ module/reach.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Memory;
+using WindowsFormsApp1.minesense.feature.Global;
+using WindowsFormsApp1.minesense.feature.overlays;
+
+namespace WindowsFormsApp1.minesense.feature.module
+{
+    public class reach
+    {               
+        public static void Enable() // enables reach, if minecraft is open
+        {
+            Form1 form = new Form1();
+            Mem m = new Mem();
+            arraylist arlst = new arraylist();
+            
+            if (m.OpenProcess("Minecraft.Windows.exe"))
+            {
+                if (form.skeetCheckbox12.Checked == true)
+                {
+                    arlst.ReachArLst.Show();
+                    arlst.reachPanel.Show();
+                    GlobalVarHandler.MODULEAMOUNT = GlobalVarHandler.MODULEAMOUNT + 1;
+                    m.WriteMemory("base+3FAE0D0", "float", Convert.ToString(form.skeetSlider6.Value));
+                    Console.WriteLine(GlobalVarHandler.module + "Reach enabled.");
+                }                                    
+            }
+            else
+            {
+                Console.WriteLine(GlobalVarHandler.module + "Please open Minecraft before attempting to enable modules.");
+                return;
+            }
+        }
+
+        public static void Disable() // disables reach
+        {
+            Form1 form = new Form1();
+            Mem m = new Mem();
+            arraylist arlst = new arraylist();
+
+            if (form.skeetCheckbox12.Checked == false)
+            {
+                arlst.ReachArLst.Hide();
+                arlst.reachPanel.Hide();
+                form.reachRandomizeTimer.Stop();
+                GlobalVarHandler.MODULEAMOUNT = GlobalVarHandler.MODULEAMOUNT - 1;
+                m.WriteMemory("base+3FAE0D0", "float", "3");
+                Console.WriteLine(GlobalVarHandler.module + "Reach disabled.");
+            }
+        }
+
+        public static void reachRandomizer() // initializes randomizer for reach
+        {
+            Form1 form = new Form1();
+            Mem m = new Mem();
+            arraylist arlst = new arraylist();
+
+            if (form.skeetCheckbox11.Checked == true && form.skeetCheckbox12.Checked == true)
+            {
+                arlst.ReachArLst.Show(); //arraylist tomfoolery - doesn't work for now, causes catasthropical and brutal error
+                arlst.reachPanel.Show();
+                form.reachRandomizeTimer.Start();
+                Console.WriteLine(GlobalVarHandler.module + "Reach randomizer active.");
+            }
+            else
+            {
+                arlst.ReachArLst.Hide();
+                form.reachRandomizeTimer.Stop(); // fuck you horion we are better
+                Console.WriteLine(GlobalVarHandler.module + "Reach randomizer disabled.");
+            }
+        }
+    }
+}
