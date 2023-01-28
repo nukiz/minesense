fe697badc9f8f03152ccbcc6bc1fa33ed9c90134
nukiz <61660487+nukiz@users.noreply.github.com>
2023-01-28 15:52:54 +0200

    Update antivoid.cs

--- client/minesense/feature/module/antivoid.cs
+++ client/minesense/feature/module/antivoid.cs
@@ -8,5 +8,11 @@ namespace WindowsFormsApp1.minesense.feature.module
 {
     public class antivoid
     {
+        //TODO: implement this aswell
+        // How it should be done (i think) 
+        // For one, we should use Memory.dll to read data from player XYZ coordinates addresses.
+        // We should then use this data to see, if we have fallen over the value specified in client block amount to snapback threshold.
+        // If this value is exceeded, we should return to original XYZ coordinates. XZ coordinates should only be mentioned in the method that will calculate Y coordinate change.
+        // You can find XYZ in LocalPlayer vtable in cheat engine. I can give a cheat table for anyone that is interested.
     }
 }
