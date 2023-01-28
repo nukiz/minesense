11077f3f960f69ac209599695c761e6ae79ad7db
nukiz <61660487+nukiz@users.noreply.github.com>
2023-01-28 15:55:32 +0200

    Update velocity.cs

--- client/minesense/feature/module/velocity.cs
+++ client/minesense/feature/module/velocity.cs
@@ -8,5 +8,9 @@ namespace WindowsFormsApp1.minesense.feature.module
 {
     public class velocity
     {
+        // This is pretty straightforward.
+        // You find knockback change memory address from somewhere (idk where, this is why this doesn't exist) and set it's value to only change X% depending on XZ/Y % specified in client.
+        // For anticombo, we should see how many times and if the times exceed a number (lets say 5). If the player has been hit more than 5 times in like 6-8 secs, we should negate all knockback for a small while.
+        // This way the player will land safely and take knockback again.
     }
 }
