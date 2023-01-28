b5f6e31cfbf80d935be158bbbcc39677ff6f7a6b
nukiz <61660487+nukiz@users.noreply.github.com>
2023-01-28 15:57:37 +0200

    Update throwpot.cs

--- client/minesense/feature/module/throwpot.cs
+++ client/minesense/feature/module/throwpot.cs
@@ -8,5 +8,9 @@ namespace WindowsFormsApp1.minesense.feature.module
 {
     public class throwpot
     {
+        // This is easy. I just never got around to making a keyboard/mouse hook for this.
+        // We have the checkboxes inside the client. We should make the client separate checkboxes 1-9 to hotbar slots.
+        // If every checkbox between 2-8 is checked, we press keys 2-8 and right click the mouse. This doesn't need anything too fancy of hooks to create.
+        // Also needs bind system to actually function.
     }
 }
