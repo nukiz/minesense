5587312af4d6870672d19b47cb9d81860f5819a9
nukiz <61660487+nukiz@users.noreply.github.com>
2023-01-28 15:50:01 +0200

    Update comebackassist.cs

--- client/minesense/feature/module/comebackassist.cs
+++ client/minesense/feature/module/comebackassist.cs
@@ -8,6 +8,10 @@ namespace WindowsFormsApp1.minesense.feature.module
 {
     public class comebackassist
     {
-
+        //TODO: this should be implemented
+        // For starters, this is how i imagine this module:
+        // When the module is enabled, we listen/start a timer for the player going lower than the HP threshold value
+        // When this happens, we set the values of reach/antikb/autoclicker higher. This shouldn't affect antivoid and so on.
+        // Then of course we fix the countless bugs, lmao!
     }
 }
